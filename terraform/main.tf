b3ad35adbfb04d51d9b1ac96f8496f38f078e69f
ngumonelson123 <nelsonmbui88@gmail.com>
2025-08-01 14:15:26 +0300

    Automated deployment update

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,11 +67,12 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = [var.allowed_ssh_cidr]
   }
 
+  # Application services (internal communication)
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   ingress {
@@ -81,11 +82,53 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["10.0.0.0/16"]
   }
 
+  # Monitoring services (public access)
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Grafana"
+  }
+
   ingress {
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Prometheus"
+  }
+
+  ingress {
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Kibana"
+  }
+
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Elasticsearch"
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "cAdvisor"
+  }
+
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Node Exporter"
   }
 
   ingress {
